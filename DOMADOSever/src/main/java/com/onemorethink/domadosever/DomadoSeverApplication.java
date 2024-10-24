7b56d2fa932831ca758536c10528fb862caf8351
fine <92whdtjs@gmail.com>
2024-10-24 23:33:44 +0900

    프로젝트 세팅 및 기본 의존성 설정

---
+++ DOMADOSever/src/main/java/com/onemorethink/domadosever/DomadoSeverApplication.java
@@ -0,0 +1,13 @@
+package com.onemorethink.domadosever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomadoSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomadoSeverApplication.class, args);
+    }
+
+}
---
+++ DOMADOSever/src/test/java/com/onemorethink/domadosever/DomadoSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onemorethink.domadosever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomadoSeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
