5be5013dbf35350478b7e5826d14f2cbc4c8a6fb
fine <92whdtjs@gmail.com>
2024-11-12 00:14:01 +0900

    HiBikeStatus 도입으로 BikeStatus 임시스테이션 케이스 삭제

--- DOMADOSever/src/main/java/com/onemorethink/domadosever/domain/bike/entity/BikeStatus.java
+++ DOMADOSever/src/main/java/com/onemorethink/domadosever/domain/bike/entity/BikeStatus.java
@@ -4,7 +4,6 @@ public enum BikeStatus {
     PARKED("주차"),
     IN_USE("사용중"),
     TEMPORARY_LOCKED("일시잠금"),
-    TEMPORARY_STATION("임시스테이션"),
     MAINTENANCE("정비중"),
     LOW_BATTERY("배터리 부족"),
     OUT_OF_SERVICE("이용 불가");
