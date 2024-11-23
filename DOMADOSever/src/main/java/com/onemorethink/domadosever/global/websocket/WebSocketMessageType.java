383750e7945de0d9236d62696a68c02bafaa7df6
fine <92whdtjs@gmail.com>
2024-11-23 14:16:14 +0900

    WebSoket 자전거 반납 관련 메시지 유형 추가

--- DOMADOSever/src/main/java/com/onemorethink/domadosever/global/websocket/WebSocketMessageType.java
+++ DOMADOSever/src/main/java/com/onemorethink/domadosever/global/websocket/WebSocketMessageType.java
@@ -1,10 +1,8 @@
 package com.onemorethink.domadosever.global.websocket;
 
 public class WebSocketMessageType {
-    public static final String RENTAL_STATUS = "RENTAL_STATUS";
-    public static final String RENTAL_START = "RENTAL_START";
-    public static final String RENTAL_END = "RENTAL_END";
-    public static final String RENTAL_PAUSE = "RENTAL_PAUSE";
+    public static final String COMPLETE_DOCKING = "COMPLETE_DOCKING";
+    public static final String COMPLETE_BIKE_TRANSFER = "COMPLETE_BIKE_TRANSFER";
     public static final String ERROR = "ERROR";
     public static final String CONNECTED = "CONNECTED";
     public static final String DISCONNECTED = "DISCONNECTED";
